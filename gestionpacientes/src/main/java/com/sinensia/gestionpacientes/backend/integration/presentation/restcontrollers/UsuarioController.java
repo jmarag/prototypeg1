5e98c540b0a83a427d9b898ca3f21ff300f67e61
VICTOR MATEOS YUSTE <0017410@insags.com>
2021-02-04 17:55:15 +0100

    usuario restcontroller creado y probado

--- gestionpacientes/src/main/java/com/sinensia/gestionpacientes/backend/integration/presentation/restcontrollers/UsuarioController.java
+++ gestionpacientes/src/main/java/com/sinensia/gestionpacientes/backend/integration/presentation/restcontrollers/UsuarioController.java
@@ -1,10 +1,14 @@
 package com.sinensia.gestionpacientes.backend.integration.presentation.restcontrollers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,15 +17,29 @@ import com.sinensia.gestionpacientes.backend.integration.repositories.UsuarioPLR
 
 @RestController
 @CrossOrigin
-@RequestMapping("/usuarios")
+@RequestMapping
 public class UsuarioController {
 
 	@Autowired
 	private UsuarioPLRepository usuarioPLRepository;
 
-	@GetMapping
+	@GetMapping("/usuarios")
 	public List<UsuarioPL> getAll() {
 
 		return usuarioPLRepository.findAll();
+	}
+
+	@GetMapping("/usuario/{dni}")
+	public UsuarioPL getByDNI(@PathVariable String dni) {
+		Optional<UsuarioPL> optionalUsuarioPL = usuarioPLRepository.findById(dni);
+		UsuarioPL usuarioPL = optionalUsuarioPL.orElse(null);
+
+		return usuarioPL;
+	}
+
+	@PostMapping("/crear-usuario")
+	public UsuarioPL create(@RequestBody UsuarioPL usuario) {
+
+		return usuarioPLRepository.save(usuario);
 	}
 }
